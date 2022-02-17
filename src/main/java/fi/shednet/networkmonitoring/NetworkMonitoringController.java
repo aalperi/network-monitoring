85c18275ba3c2405dca76892b39dbb175bb427e2
aalperi <ahlberg.heidi@gmail.com>
2022-02-17 13:08:46 +0200

    first attempt on pageable view

--- src/main/java/fi/shednet/networkmonitoring/NetworkMonitoringController.java
+++ src/main/java/fi/shednet/networkmonitoring/NetworkMonitoringController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class NetworkMonitoringController {
@@ -27,5 +29,13 @@ public class NetworkMonitoringController {
         model.addAttribute("downloadUploadResults", this.networkMonitoringService.getDownloadUpload(useCase));
         model.addAttribute("pingResults", this.networkMonitoringService.getPing(useCase));
         return "all";
+    }
+    @GetMapping("/page")
+    public String index(Model model, @RequestParam("page") Optional<Integer> page) {
+        int currentPage = page.orElse(1);
+        model.addAttribute("results", this.networkMonitoringService.getPageableMonitoringEvents(currentPage));
+        model.addAttribute("downloadUploadResults", this.networkMonitoringService.getPageableDownloadUpload(currentPage));
+        model.addAttribute("pingResults", this.networkMonitoringService.getPageablePing(currentPage));
+        return "index";
     }
 }
--- src/main/java/fi/shednet/networkmonitoring/NetworkMonitoringService.java
+++ src/main/java/fi/shednet/networkmonitoring/NetworkMonitoringService.java
@@ -1,8 +1,11 @@
 package fi.shednet.networkmonitoring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import org.springframework.data.domain.Pageable;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
@@ -11,6 +14,7 @@ import java.util.List;
 @Service
 public class NetworkMonitoringService {
 
+    int PAGE_SIZE = 576;
     @Autowired
     private MonitoringEntityRepository monitoringEntityRepository;
 
@@ -19,6 +23,12 @@ public class NetworkMonitoringService {
             return this.monitoringEntityRepository.findAll();
         } else
             return this.monitoringEntityRepository.findFirst576ByOrderByTimestampDesc();
+    }
+
+    public List<MonitoringEntityModel> getPageableMonitoringEvents(Integer page){
+        Pageable pageable = PageRequest.of(page-1,PAGE_SIZE);
+        Page events=this.monitoringEntityRepository.findAll(pageable);
+        return events.getContent();
     }
 
     public List<List<Object>> getDownloadUpload(String useCase) {
@@ -52,6 +62,58 @@ public class NetworkMonitoringService {
 
     public List<List<Object>> getPing(String useCase) {
         List<MonitoringEntityModel> all = getMonitoringEvents(useCase);
+        if (!all.isEmpty()) {
+            BigDecimal sumPing = BigDecimal.valueOf(0);
+            for (int i = 0; i < all.size(); i++) {
+                sumPing=sumPing.add(all.get(i).getPing());
+            }
+            List<List<Object>> entities = new ArrayList<>();
+            for (int i = 0; i < all.size(); i++) {
+                if (all.get(i) != null) {
+                    if (all.get(i).getTimestamp() != null) {
+                        entities.add(List.of(
+                                all.get(i).getTimestamp().toInstant().toEpochMilli(),
+                                all.get(i).getPing(),
+                                sumPing.divide(BigDecimal.valueOf(all.size()),2, RoundingMode.HALF_UP)
+                        ));
+                    }
+                }
+            }
+            return entities;
+        }
+        return null;
+    }
+    public List<List<Object>> getPageableDownloadUpload(Integer page) {
+        List<MonitoringEntityModel> all = getPageableMonitoringEvents(page);
+        if (!all.isEmpty()) {
+            List<List<Object>> entities = new ArrayList<>();
+            BigDecimal sumDownload = BigDecimal.valueOf(0);
+            BigDecimal sumUpload = BigDecimal.valueOf(0);
+            for (int i = 0; i < all.size(); i++) {
+                sumDownload=sumDownload.add(all.get(i).getDownload().divide(BigDecimal.valueOf(1000000L),2, RoundingMode.HALF_UP));
+                sumUpload=sumUpload.add(all.get(i).getUpload().divide(BigDecimal.valueOf(1000000L),2, RoundingMode.HALF_UP));
+            }
+            for (int i = 0; i < all.size(); i++) {
+                if (all.get(i) != null) {
+                    if (all.get(i).getTimestamp() != null) {
+                        entities.add(List.of(
+                                all.get(i).getTimestamp().toInstant().toEpochMilli(),
+                                all.get(i).getDownload().divide(BigDecimal.valueOf(1000000L),2, RoundingMode.HALF_UP),
+                                all.get(i).getUpload().divide(BigDecimal.valueOf(1000000L),2, RoundingMode.HALF_UP),
+                                sumDownload.divide(BigDecimal.valueOf(all.size()),2, RoundingMode.HALF_UP),
+                                sumUpload.divide(BigDecimal.valueOf(all.size()),2, RoundingMode.HALF_UP)
+                        ));
+
+                    }
+                }
+            }
+            return entities;
+        }
+        return null;
+    }
+
+    public List<List<Object>> getPageablePing(Integer page) {
+        List<MonitoringEntityModel> all = getPageableMonitoringEvents(page);
         if (!all.isEmpty()) {
             BigDecimal sumPing = BigDecimal.valueOf(0);
             for (int i = 0; i < all.size(); i++) {
